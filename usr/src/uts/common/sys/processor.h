0e74bd6c81b490b14a659fd2c1976d8c4d9fc530
Ryan Zezeski <ryan@zinascii.com>
2016-01-05 16:44:38 +0000

    WIP: add declaration

--- usr/src/uts/common/sys/processor.h
+++ usr/src/uts/common/sys/processor.h
@@ -135,6 +135,8 @@ extern int	processor_info(processorid_t processorid,
 		    processor_info_t *infop);
 extern int	processor_bind(idtype_t idtype, id_t id,
 		    processorid_t processorid, processorid_t *obind);
+extern int	processor_bind2(pbind2_op_t, idtype_t idtype, id_t id,
+    size_t ncpus, processorid_t *cpus, uchar_t *flags);
 extern processorid_t getcpuid(void);
 extern lgrpid_t gethomelgroup(void);
 
