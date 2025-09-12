9c168982ea18ff9054f0af7bbc6947e7c09af38e
Ryan Zezeski <ryan@zinascii.com>
2025-09-12 17:52:43 -0400

    disable WC UDB when QID exceeds UDB page limit
    Change-Id: I78532329a1be360c484c3ae437f4271cd55745ff

--- usr/src/uts/common/io/cxgbe/t4nex/t4_sge.c
+++ usr/src/uts/common/io/cxgbe/t4nex/t4_sge.c
@@ -1575,9 +1575,33 @@ alloc_eq(struct adapter *sc, struct port_info *pi, struct sge_eq *eq)
 		rc = t4_bar2_sge_qregs(sc, eq->cntxt_id, T4_BAR2_QTYPE_EGRESS,
 		    0, &udb_offset, &udb_qid);
 
+		/*
+		 * RPZ: This looks like a bug: in the rc == 0 case, the case
+		 * where things were successful and we setup UDBs, we are
+		 * setting the udb_qid, but in t4_tx_ring_db we assert that it's
+		 * zero. How would this ever work? I'm beginning to wonder if
+		 * Patrick never execersized the `eq->pending == 1` path in
+		 * t4_tx_ring_db().
+		 *
+		 * Oh wait, it depends on the type of UDB. The manual said you
+		 * can either use the segment offset or write the relative QID.
+		 * Perhaps on T7 the chip told the driver to use the QID?
+		 */
 		if (rc == 0) {
 			eq->udb = sc->bar2_ptr + udb_offset;
 			eq->udb_qid = udb_qid;
+
+			/*
+			 * In this case we have UDB capability but we cannot use
+			 * Write-Combining for this Egress Queue because it's
+			 * relative QID is greater than the number of doorbell
+			 * segments for a page. In this case we fallback to the
+			 * simple method of writing the relative QID into the
+			 * first UDB segment of the UDB page.
+			 */
+			if (eq->udb_qid != 0) {
+				eq->doorbells &= ~DOORBELL_WCWR;
+			}
 		} else {
 			eq->doorbells &=
 			    ~(DOORBELL_UDB | DOORBELL_UDBWC | DOORBELL_WCWR);
@@ -2998,6 +3022,8 @@ t4_tx_ring_db(struct sge_txq *txq)
 	else
 		val = V_PIDX_T5(eq->pending);
 
+	/* RPZ: Is there an intrinsic we could use to determine the bit, or
+	 * maybe a way to cache this to avoid doing this every time? */
 	db_mode = (1 << (ffs(db) - 1));
 	switch (db_mode) {
 		case DOORBELL_WCWR: {
